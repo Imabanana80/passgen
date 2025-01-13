7cd2d16c3fa40b6aaff1d3e60f062959766e6f57
Imabanana80 <imabanana80.hallo@gmail.com>
2025-01-13 19:04:27 +0800

    feat: replace wordlist with better words

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/atotto/clipboard"
 )
 
-//go:embed wordsA.txt
-var wordsA string
+//go:embed words.txt
+var words string
 
 func main() {
     random := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -19,19 +19,18 @@ func main() {
 }
 
 func gen(random *rand.Rand) {
-    var lines []string = strings.Split(strings.ReplaceAll(wordsA, "\r\n", "\n"), "\n")
+    var lines []string = strings.Split(strings.ReplaceAll(words, "\r\n", "\n"), "\n")
     var chars = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
         "!", "?", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "=", "/", "~"}
     var caps = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
     password := ""
-    rounds := 4 + random.Intn(2);  
-    for i := 0; i<rounds; i++ {
+    for i := 0; i<4; i++ {
         password += lines[random.Intn(len(lines))]
-        for o := 0; o < random.Intn(2); o++ {
+        for o := 0; o < random.Intn(1); o++ {
             password += caps[random.Intn(len(caps))]
         }
         password += chars[random.Intn(len(chars))]
-        for o := 0; o < random.Intn(3); o++ {
+        for o := 0; o < random.Intn(2); o++ {
             password += chars[random.Intn(len(chars))]
         }
     }
